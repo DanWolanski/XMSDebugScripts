27c4c712adccee2748e637dcde7e7beb7f39be2d
Dan Wolanski <wolanskd@yahoo.com>
2016-06-05 21:23:38 -0400

    Updated monitor and gencore script

--- genXMScores.sh
+++ genXMScores.sh
@@ -3,32 +3,162 @@
 starttime=`date +"%Y-%m-%d_%H-%M-%S"`
 coredir="xmscores-$starttime"
 
-mkdir $coredir
+. /etc/init.d/functions
+LOG="genXMScores.log"
+STARTTIME=`date +"%y-%m-%d_%H-%m-%s"` 
+echo $STARTTIME > $LOG
+echo `hostname` >> $LOG
 
+
+# Use step(), try(), and next() to perform a series of commands and print
+# [  OK  ] or [FAILED] at the end. The step as a whole fails if any individual
+# command fails.
+#
+step() {
+    echo -n -e "$@"
+
+    
+    echo -e "\n\nSTEP -  $@">> $LOG
+    STEP_OK=0
+    [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+}
+
+try() {
+    # Check for `-b' argument to run command in the background.
+    local BG=
+
+    [[ $1 == -b ]] && { BG=1; shift; }
+    [[ $1 == -- ]] && {       shift; }
+
+    # Run the command.
+    echo -e "$@" >> $LOG
+
+    if [[ -z $BG ]]; then
+        "$@" &>> $LOG
+    else
+        "$@" & &>> $LOG
+    fi
+
+    # Check if command failed and update $STEP_OK if so.
+    local EXIT_CODE=$?
+
+    if [[ $EXIT_CODE -ne 0 ]]; then
+        STEP_OK=$EXIT_CODE
+        [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+
+        if [[ -n $LOG_STEPS ]]; then
+            local FILE=$(readlink -m "${BASH_SOURCE[1]}")
+            local LINE=${BASH_LINENO[0]}
+
+            echo "$FILE: line $LINE: Command \'$*\' failed with exit code $EXIT_CODE." >> "$LOG_STEPS"
+        fi
+    fi
+
+    return $EXIT_CODE
+}
+
+next() {
+    [[ -f /tmp/step.$$ ]] && { STEP_OK=$(< /tmp/step.$$); rm -f /tmp/step.$$; }
+    [[ $STEP_OK -eq 0 ]]  && echo_success || echo_failure
+    echo
+
+    return $STEP_OK
+}
+
+setpass() {
+    echo -n "$@"
+
+    STEP_OK=0
+    [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+}
+setfail() {
+    echo -n "$@"
+
+    STEP_OK=1
+    [[ -w /tmp ]] && echo $STEP_OK > /tmp/step.$$
+}
+
+genBT(){
+
+    echo Generated via btdumpscript.sh > $coredir/corebtinfo.log
+    echo Core List in dir - >> $coredir/corebtinfo.log 
+echo `ls -al ./$coredir/*_core.*` >> $coredir/corebtinfo.log
+echo ------------------------------- >> $coredir/corebtinfo.log
+for corefile in ./$coredir/*_core.*
+do
+
+	echo ============================= NEXT {$corefile}  ========================================== &>> $coredir/corebtinfo.log
+	exe= `file ${corefile}  | grep -oE "'.*?'" | grep -oE "/.*?'" | rev | cut -c2- | rev` &>> $coredir/corebtinfo.log
+	echo ---------------------------------------------------------------------------- &>> $coredir/corebtinfo.log
+	echo `ls -al $corefile` &>> $coredir/corebtinfo.log
+	echo `file $corefile` &>> $coredir/corebtinfo.log
+	echo ---------------------------------------------------------------------------- &>> $coredir/corebtinfo.log
+	echo BT only &>> $coredir/corebtinfo.log
+	echo ---------------------------------------------------------------------------- &>> $coredir/corebtinfo.log
+	gdb --batch --quiet -ex "bt " -ex "quit" -core ${corefile} ${exe} &>>  $coredir/corebtinfo.log
+	echo ---------------------------------------------------------------------------- &>> $coredir/corebtinfo.log
+	echo FULL trace on all threads  &>> $coredir/corebtinfo.log
+	echo ---------------------------------------------------------------------------- &>> $coredir/corebtinfo.log
+	gdb --batch --quiet -ex "thread apply all bt full" -ex "quit" -core ${corefile} ${exe} &>>  $coredir/corebtinfo.log
+done
+
+	echo ============================= DONE  ========================================== &>> $coredir/corebtinfo.log
+
+
+}
+logger "User has intiated generation of XMS process cores via genXMScores.sh"
+
+step "Creating core output directory"
+try mkdir $coredir
+next
+
+
+if [[ -x "./xmsinfo.sh" ]]
+then
+    step "Obtaining XMSInfo"
+    try ./xmsinfo.sh $coredir/xmsinfo-$starttime.tgz &>/dev/null 
+    next
+fi
+
+step "Generating Cores for XMS process"
 #XMS Core processes
-gcore -o ./$coredir/xmserver_core `pidof xmserver`
-gcore -o ./$coredir/appmanager `pidof appmanager`
-gcore -o ./$coredir/broker `pidof broker`
-gcore -o ./$coredir/eventmanager `pidof eventmanager`
-gcore -o ./$coredir/nodecontroller `pidof nodecontroller`
-
+try gcore -o ./$coredir/xmserver_core_core `pidof xmserver`
+try gcore -o ./$coredir/appmanager_core `pidof appmanager`
+try gcore -o ./$coredir/broker_core `pidof broker`
+try gcore -o ./$coredir/eventmanager_core `pidof eventmanager`
+try gcore -o ./$coredir/nodecontroller_core `pidof nodecontroller`
+next
 
 #WebRTC process
-gcore -o ./$coredir/rtcweb `pidof rtcweb`
-
+step "Generating Cores for Webrtc process"
+try gcore -o ./$coredir/rtcweb_core `pidof rtcweb`
+next
 
 #HMP Core process
-gcore -o ./$coredir/ssp_x86Linux_boot `pidof ssp_x86Linux_boot`
+step "Generating Cores for Media Cores (HMP) process"
+try gcore -o ./$coredir/ssp_x86Linux_boot_core `pidof ssp_x86Linux_boot`
+next
 
 #XMS interface processes
-#gcore -o ./$coredir/vxmlinterpreter `pidof vxmlinterpreter`
-#gcore -o ./$coredir/xmsrest `pidof xmsrest`
-gcore -o ./$coredir/msml_main `pidof msmlserver`
-#gcore -o ./$coredir/netann `pidof netann`
+step "Generating Cores for XMS Control Interface process"
+try gcore -o ./$coredir/vxmlinterpreter_core `pidof vxmlinterpreter`
+try gcore -o ./$coredir/xmsrest_core `pidof xmsrest`
+try gcore -o ./$coredir/msml_main_core `pidof msmlserver`
+try gcore -o ./$coredir/netann_core `pidof netann`
+next
 
+step "Gathering executable binaries"
 #copy of the binaries
-tar cvzf ./$coredir/binaries.tgz /usr/dialogic/bin/ssp_x86Linux_boot /usr/bin/xmserver /usr/bin/appmanager /usr/bin/broker /usr/bin/eventmanager /usr/bin/nodecontroller /usr/bin/rtcweb /usr/bin/vxmlinterpreter /usr/bin/xmsrest /usr/bin/msmlserver /usr/bin/netann 
+try tar cvzf ./$coredir/binaries.tgz /usr/dialogic/bin/ssp_x86Linux_boot /usr/bin/xmserver /usr/bin/appmanager /usr/bin/broker /usr/bin/eventmanager /usr/bin/nodecontroller /usr/bin/rtcweb /usr/bin/vxmlinterpreter /usr/bin/xmsrest /usr/bin/msmlserver /usr/bin/netann  &> /dev/null
+next
 
-echo "----------------------------------------------------------------"
-echo  Cores available in $coredir 
-echo "----------------------------------------------------------------"
+step "Generating Backtraces from cores"
+genBT
+setpass
+next
+
+echo "----------------------------------------------------------------" | tee -a $LOG
+echo  Cores available in $coredir | tee -a $LOG
+echo "----------------------------------------------------------------" | tee -a $LOG
+
+#TODO, put detect in for abrt logs or cores already available
--- monitor.sh
+++ monitor.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
+#will do the compress every ~240 x 15 or 3600 seconds (1hour)
+COUNTTILLZIP=240
+SLEEPTIME=15
 
+trap onExit INT SIGHUP SIGINT SIGTERM
+
+function onExit() {
+    clear
+    echo "Exit Trap Received"
+    echo "Generating final tgz monitor-$starttime-final.tgz"
+	sar -A > sar-output.txt
+    tar cvzf "monitor-$starttime-final.tgz" *-output.txt
+    echo 'Clearing temp files'
+       rm -f tmptop.txt
+       rm -f tmpps.txt
+       rm -f sar-output.txt
+       rm -f top-output.txt
+       rm -f ps-output.txt
+       rm -f meters-output.txt
+       rm -f topthreads-output.txt
+       rm -f tmp-network.txt
+    echo -e "Cleanup complete!\n"
+    echo -e "Exiting\n\n"
+    exit 
+}
+
+#Put here anything that should be done once per script run
 #start network capture 
 # Comment out line with tcpdump if you don't want network tracing 
-echo -e "Starting background network trace \n" 
+echo -e "Starting background network trace for 4xx and 5xx errors\n" 
 rm -f tmp-network.txt
 tcpdump -i any -v port 5060 | grep -P "SIP/2.0 [4|5]" > tmp-network.txt &
 
 while true
 do
        LOOPCOUNTER=0
+       #clean up the temp files used as old ones are no saved in tgz
        echo 'Clearing temp files'
        rm -f tmptop.txt
        rm -f tmpps.txt
@@ -17,17 +44,17 @@ do
        rm -f ps-output.txt
        rm -f meters-output.txt
        rm -f topthreads-output.txt
-
-       starttime=`date +"%y-%m-%d_%H:%M:%S.%3N"`
+       
+       starttime=`date +"%y_%m_%d-%H_%M_%S.%3N"`
        hostname=`hostname`
-       #will do the compress every ~10mins
-       while [ $LOOPCOUNTER -lt 120 ]; do
+       
+       while [ $LOOPCOUNTER -lt $COUNTTILLZIP ]; do
        let LOOPCOUNTER=LOOPCOUNTER+1
        clear
        looptimestamp=`date +"%y-%m-%d_%H:%M:%S.%3N"`
-       echo -e $looptimestamp "-" $hostname [$LOOPCOUNTER]"\n"
+       echo -e $looptimestamp "-" $hostname [$LOOPCOUNTER out of $COUNTTILLZIP]"\n"
        echo -e "\nDisk/Mem Usage:"
-       free -m
+         free -m
        #echo -e "\n"
        # df
        #echo -e "\n\n\nUptime:" `uptime`
@@ -39,67 +66,79 @@ do
 	    echo -e $looptime stamp "----------------"  > tmpps.txt
 	    ps -A  -Lo %cpu,pid,lwp,comm=,args >> tmpps.txt
 	    
-        echo -e "\n\nCPU Intensive Threads"
-	    grep '^ *[0-9][0-9][0-9]\.' tmpps.txt
-        grep '^ *[2-9][0-9]\.' tmpps.txt
+        echo -e "\n\nCPU Intensive Threads:"
+	      grep -P '^ *[0-9][0-9][0-9]\.' tmpps.txt
+          grep -P '^ *[4-9][0-9]\.' tmpps.txt
+          
         echo -e "\n\nTop Info:"
-        grep Cpu tmptop.txt 
-        grep Mem: tmptop.txt
-        grep Swap: tmptop.txt
+          grep Cpu tmptop.txt 
+          grep Mem: tmptop.txt
+          grep Swap: tmptop.txt
 	    echo -e "\n"
-	    grep PID tmptop.txt
-        grep ssp tmptop.txt
-        grep appmanager tmptop.txt
-        grep xmserver tmptop.txt
+	      grep PID tmptop.txt
+          grep ssp tmptop.txt
+          grep appmanager tmptop.txt
+          grep xmserver tmptop.txt
         
         #these can be commended out based on tech used
-        grep msml tmptop.txt
-        grep vxml tmptop.txt
-        grep rest tmptop.txt
-        grep netann tmptop.txt
+          grep msml tmptop.txt
+          grep vxml tmptop.txt
+          grep rest tmptop.txt
+          grep netann tmptop.txt
         
-        grep httpclient tmptop.txt
+          grep httpclient tmptop.txt
 
         #used by mrb/lb
-        grep java tmptop.txt
+        #  grep java tmptop.txt
         
         #Alternatively you can use
         #top -n 1 -b | head -20
 
 	    
         echo -e "\n\nMeters(if available):"
-        grep xmsRtpSessions /var/lib/xms/meters/currentValue.txt
-        grep xmsSignalingSessions /var/lib/xms/meters/currentValue.txt
-#        grep calls.active /var/lib/xms/meters/currentValue.txt
-#        grep transactions.active /var/lib/xms/meters/currentValue.txt
+          grep xmsRtpSessions /var/lib/xms/meters/currentValue.txt
+          grep xmsSignalingSessions /var/lib/xms/meters/currentValue.txt
+          grep calls.active /var/lib/xms/meters/currentValue.txt
+          grep xmsMediaTransactions /var/lib/xms/meters/currentValue.txt
+#        grep xmsLicenseUsage /var/lib/xms/meters/currentValue.txt
 
 #	mpstat -P ALL 1 | tee mpstats.txt
 
-        if [ -f tmp-network.txt ] ;
+        if [[ -f tmp-network.txt ]] ;
         then
-            echo -e "\nNetwork Error count:" + `wc -l tmp-network.txt` + "\n"
-            #cat tmp-network.txt
+            echo -e "\n\nNetwork Error count:" `cat tmp-network.txt | wc -l ` "\n"
+            #cat tmp-network.txt >> networkerror-output.txt
             cat /dev/null > tmp-network.txt
         fi
         
-        echo -e  "$looptime-$hostname\n" >> top-output.txt
+        echo -e  "\n$looptime-$hostname\n" >> top-output.txt
         cat tmptop.txt >> top-output.txt
-        echo -e  "$looptime-$hostname\n" >> ps-output.txt
+        echo -e  "\n$looptime-$hostname\n" >> ps-output.txt
 	    cat tmpps.txt >> ps-output.txt
-        echo -e "$looptime-$hostname\n" >> meters-output.txt
+        echo -e "\n$looptime-$hostname\n" >> meters-output.txt
         cat /var/lib/xms/meters/currentValue.txt >> meters-output.txt
        
-        echo -e  "$looptime-$hostname\n" >> topthreads-output.txt
+        echo -e  "\n$looptime-$hostname\n" >> topthreads-output.txt
         top -b -n 1 -H >> topthreads-output.txt
 	
+        
+        ## additional activites or spy can be done here such as watching messagefile or netcap for 15 seconds or looking for errors to print
+        #timeout $SLEEPTIME tail -F -n0 tmp-network.txt
+        #timeout $SLEEPTIME ./scripttorunfortime.sh
+        echo -e "\n\n"
+        sleep $SLEEPTIME
+
         rm -f tmptop.txt
         rm -f tmpps.txt
-	
-        echo -e "\n\n"
-        sleep 5 
+	        
         done
+##put all things there that need to be gathered once per compress loop
+
 	sar -A > sar-output.txt
-	tar cvzf monitor-$starttime.tgz *-output.txt
-    #delete files over 2 days old   
-    find . -name 'monitor-*.tgz' -mtime +3 -delete
+
+    tar cvzf "monitor-$starttime.tgz" *-output.txt
+    #delete files over 7 days old   
+    find . -name 'monitor-*.tgz' -mtime +7 -delete
 done
+
+onExit
