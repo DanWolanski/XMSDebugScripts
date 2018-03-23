f95c3307827cac8ba3c3a5fa69add40d0afeebe0
Dan Wolanski <wolanskd@yahoo.com>
2018-03-23 15:27:19 -0400

    Fixed it right this time

--- splitstereo.sh
+++ splitstereo.sh
@@ -6,7 +6,7 @@ for f in $FILES
 do
 echo "Processing $f"
 echo "  - left -> l_${f}"
-sox $f l_$f.wav remix 1
+sox $f l_$f remix 1
 echo "  - right -> r_${f}"
-sox $f r_$f.wav remix 2
+sox $f r_$f remix 2
 done
